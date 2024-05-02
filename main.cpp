f975b035db6f5f58237af0ce1dddac168e38e146
ArtemAnistratov <mne.ne.nuzgen.akk@gmail.com>
2024-05-02 14:10:38 +0300

    ex2 was done

--- main.cpp
+++ main.cpp
@@ -4,9 +4,20 @@ using namespace std;
 int main()
 {
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, 0x0C);
+	cout << "Every\n";
+	SetConsoleTextAttribute(hConsole, 0x0C);
+	cout << "hunter\n";
 	SetConsoleTextAttribute(hConsole, 0x0E);
-	cout << "\"Hellow? World!\"" << endl;
+	cout << "wants\n";
+	SetConsoleTextAttribute(hConsole, 0x0A);
+	cout << "to know\n";
+	SetConsoleTextAttribute(hConsole, 0x09);
+	cout << "where\n";
+	SetConsoleTextAttribute(hConsole, 0x01);
+	cout << "the pheasant\n";
+	SetConsoleTextAttribute(hConsole, 0x0D);
+	cout << "is.\n";
 	SetConsoleTextAttribute(hConsole, 0x0F);
-	cout << "/Bjarne Stroustrup/\n";
 	return 0;
 }

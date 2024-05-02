ee63b230b574500ab2fae205d4f1e3319b5dc8e3
ArtemAnistratov <mne.ne.nuzgen.akk@gmail.com>
2024-05-02 13:12:24 +0300

    ex2 was done

--- main.cpp
+++ main.cpp
@@ -4,9 +4,8 @@ using namespace std;
 int main()
 {
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hConsole, 0x0E);
-	cout << "\"Hellow? World!\"" << endl;
 	SetConsoleTextAttribute(hConsole, 0x0F);
-	cout << "/Bjarne Stroustrup/\n";
+	cout << "Hellow World!" << endl;
+	cout << "\\\\Dennis Ritchi\\\\" << endl;
 	return 0;
 }

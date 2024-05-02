53393d838718d3dd41b81c6ec4698dfb8420261b
ArtemAnistratov <mne.ne.nuzgen.akk@gmail.com>
2024-05-02 13:50:15 +0300

    ex1 was done

--- main.cpp
+++ main.cpp
@@ -3,10 +3,7 @@ using namespace std;
 #include <Windows.h>
 int main()
 {
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hConsole, 0x0E);
-	cout << "\"Hellow? World!\"" << endl;
-	SetConsoleTextAttribute(hConsole, 0x0F);
-	cout << "/Bjarne Stroustrup/\n";
+	cout << "When I am dead, bury me\nIn my beloved Ukraine,\nMy tomb upon a grave mound high\nAmid the spreading plain,\nSo that the fields, the boundless steppes,\nThe Dnieper's plunging shore\nMy eyes could see, my ears could hear\nThe mighty river roar.\n";
+	cout << "Taras Shevchenko\n";
 	return 0;
 }

4b359b05a21e19337871f965437595a95167e343
ArtemAnistratov <mne.ne.nuzgen.akk@gmail.com>
2024-05-02 13:31:25 +0300

    ex3 was done

--- main.cpp
+++ main.cpp
@@ -4,9 +4,15 @@ using namespace std;
 int main()
 {
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hConsole, 0x0E);
-	cout << "\"Hellow? World!\"" << endl;
 	SetConsoleTextAttribute(hConsole, 0x0F);
-	cout << "/Bjarne Stroustrup/\n";
+	cout << "To be,";
+	SetConsoleTextAttribute(hConsole, 0x0C);
+	cout << " or not ";
+	SetConsoleTextAttribute(hConsole, 0x0F);
+	cout << "to be: that is the question:\n";
+	cout << "Whether ";
+	SetConsoleTextAttribute(hConsole, 0x0E);
+	cout << "'tis nobler in the mind to suffer\nThe slings and arrows of outrageous fortune,\nOr to take arms against a sea of troubles,\nAnd by opposing end them ?\nWilliam Shakespeare\n";
+	SetConsoleTextAttribute(hConsole, 0x0F);
 	return 0;
 }

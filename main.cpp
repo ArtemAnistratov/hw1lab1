6d0bbd73b60c7d960e57006ffd1eb9e12cb7b735
ArtemAnistratov <mne.ne.nuzgen.akk@gmail.com>
2024-05-02 14:31:46 +0300

    ex3 was done

--- main.cpp
+++ main.cpp
@@ -4,9 +4,25 @@ using namespace std;
 int main()
 {
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	cout << "Stone for sale\n";
+	cout << "found on Mount Hoverla\n";
+	cout << "Weight     |  ";
 	SetConsoleTextAttribute(hConsole, 0x0E);
-	cout << "\"Hellow? World!\"" << endl;
+	cout << "200g";
 	SetConsoleTextAttribute(hConsole, 0x0F);
-	cout << "/Bjarne Stroustrup/\n";
+	cout << "  |\n";
+
+	cout << "State      |  ";
+	SetConsoleTextAttribute(hConsole, 0x0E);
+	cout << "good";
+	SetConsoleTextAttribute(hConsole, 0x0F);
+	cout << "  |\n";
+
+	cout << "scent      |  ";
+	SetConsoleTextAttribute(hConsole, 0x0E);
+	cout << "nice";
+	SetConsoleTextAttribute(hConsole, 0x0F);
+	cout << "  |\n";
+	cout << "call: 000 000\n";
 	return 0;
 }
